69ef11558e0efbf829f7b3615800e997a068b7bf
Riddhesh <135740066+404reese@users.noreply.github.com>
2026-04-03 14:43:35 +0530

    botadd

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/mind-matrix-api': {
+        target: 'https://api.mind-matrix.in',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/mind-matrix-api/, '')
+      }
+    }
+  }
 })
